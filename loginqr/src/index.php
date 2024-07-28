e5cd60628b5aa37511e514052aef98079480e992
JP-jirawat <141437332+JP-jirawat@users.noreply.github.com>
2024-07-28 20:55:26 +0700

    Initial commit

---
+++ loginqr/src/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="th">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+<body>
+<?php
+    include("layout/navbar.php");
+?>  
+<div class="card" style="width: 36rem;">
+  <img src="https://scontent.fbkk29-1.fna.fbcdn.net/v/t39.30808-6/342380657_247656284461489_983208231865697139_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=RIVUBVYLragQ7kNvgHwwaQI&_nc_ht=scontent.fbkk29-1.fna&gid=AGSJH-EK1TLeNVqsd0VYKXt&oh=00_AYDdaqwxAxdYSVH-SXksDTRAzBj-XWdcfjBGw6yqHhinTw&oe=66AC3820">
+  <div class="card-body">
+    <h5 class="card-title">id: 65130029</h5>
+    <p class="card-text">name: jirawat Nopchaiaunmuychock</p>
+    <a href="#" class="btn btn-primary">Hello World!</a>
+  </div>
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
